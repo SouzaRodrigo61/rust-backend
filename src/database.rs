c89721233b1eacec8312bb64fc317bb027287517
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-05-17 18:12:33 -0300

    feat: database validate connection

--- src/database.rs
+++ src/database.rs
@@ -11,10 +11,14 @@ lazy_static! {
     let db_name = SETTINGS.database.name.as_str();
 
     println!("Connecting to MongoDB...");
+    println!("DB URI: {}", db_uri);
+    println!("DB Name: {}", db_name);
 
     let client_options =
     ClientOptions::parse(db_uri).await.expect("Failed to parse MongoDB connection string");
-    
+
+    println!("Connected to MongoDB...");
+
     mongodb::Client::with_options(client_options)
       .expect("Failed to initialize MongoDB connection")
       .database(db_name)
