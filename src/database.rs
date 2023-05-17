0a4b9175eb5d1301e1381e55d7414498d6f24d1d
Rodrigo Santos de Souza <souza.rodrigo61@gmail.com>
2023-05-17 17:49:20 -0300

    feat: change connector for mongodb

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,7 @@
 use async_once::AsyncOnce;
 use lazy_static::lazy_static;
 use mongodb::Database;
-use wither::mongodb;
+use wither::mongodb::{self, options::ClientOptions};
 
 use crate::settings::SETTINGS;
 
@@ -10,8 +10,12 @@ lazy_static! {
     let db_uri = SETTINGS.database.uri.as_str();
     let db_name = SETTINGS.database.name.as_str();
 
-    mongodb::Client::with_uri_str(db_uri)
-      .await
+    println!("Connecting to MongoDB...");
+
+    let client_options =
+    ClientOptions::parse(db_uri).await.expect("Failed to parse MongoDB connection string");
+    
+    mongodb::Client::with_options(client_options)
       .expect("Failed to initialize MongoDB connection")
       .database(db_name)
   });
